a0c709402c8876a57a116058f3084b7a43b548da
Quang Nguyễn <207350182+nngocquangUIT24730135@users.noreply.github.com>
2025-04-21 20:52:35 +0700

    Tao moi class Snake

--- game.py
+++ game.py
@@ -1,68 +1,51 @@
 import os
 import time
-import msvcrt
-import sys
+import keyboard
+import random
 
-# Hàm di chuyển con trỏ console (thay thế gotoxy)
-def gotoxy(x, y):
-    print(f"\033[{y};{x}H", end='')
+# Cài đặt trò chơi
+SNAKE_CHAR = 'O'
+HEAD_CHAR = '1'
+EMPTY_CHAR = ' '
 
-# Lớp Point để lưu tọa độ
-class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+class Snake:
+    def __init__(self, body=[(5, 5)], direction=(1, 0)):
+        self.body = body           # Khởi tạo thân rắn với vị trí đầu tiên
+        self.direction = direction       # Hướng di chuyển ban đầu
 
-# Lớp CONRAN
-class CONRAN:
-    def __init__(self):
-        self.DoDai = 3
-        self.A = [Point(10, 10), Point(11, 10), Point(12, 10)]  # Khởi tạo rắn với 3 điểm
+    #TODO: Hiện thực hoá hàm di chuyển con rắn
+    def move(self, food_position):
+        # Di chuyển rắn
+        
+        # Kiểm tra ăn thức ăn
+        return True
 
-    def Ve(self):
-        # Xóa màn hình
-        os.system('cls' if os.name == 'nt' else 'clear')
-        # Vẽ rắn
-        for i in range(self.DoDai):
-            gotoxy(self.A[i].x, self.A[i].y)
-            print('X', end='')
-        sys.stdout.flush()
-
-    def DiChuyen(self, Huong):
-        # Di chuyển các điểm của rắn
-        for i in range(self.DoDai - 1, 0, -1):
-            self.A[i].x = self.A[i - 1].x
-            self.A[i].y = self.A[i - 1].y
-        # Cập nhật đầu rắn theo hướng
-        if Huong == 0:  # Phải
-            self.A[0].x += 1
-        elif Huong == 1:  # Xuống
-            self.A[0].y += 1
-        elif Huong == 2:  # Trái
-            self.A[0].x -= 1
-        elif Huong == 3:  # Lên
-            self.A[0].y -= 1
-
+    #TODO: Hiện thực hoá hàm kiểm tra va chạm
+    def check_collision(self):
+        # Kiểm tra va chạm với chính mình hoặc tường
+        return True
+    
 def main():
-    r = CONRAN()
-    Huong = 0  # Hướng ban đầu: 0 (phải), 1 (xuống), 2 (trái), 3 (lên)
+    # Tạo một con rắn với đối số mặc định
+    snake = Snake()
+    print(snake.body)
+    print(snake.direction)
 
-    while True:
-        # Kiểm tra đầu vào từ bàn phím
-        if msvcrt.kbhit():
-            t = msvcrt.getch().decode('utf-8').lower()
-            if t == 'a':
-                Huong = 2
-            elif t == 'w':
-                Huong = 3
-            elif t == 'd':
-                Huong = 0
-            elif t == 's':
-                Huong = 1
+    # Kiểm tra thông tin về kiểu dữ liệu
+    print(type(snake.body))  # <class 'list'
+    print(type(snake.body[0])) # <class 'tuple'>
+    print(type(snake.direction)) # <class 'tuple'>
 
-        r.Ve()
-        r.DiChuyen(Huong)
-        time.sleep(0.3)  # Tạm dừng 300ms
+    # Tạo con rắn thứ 2 có độ dài là 3 và hướng di chuyển sang trái
+    snake2 = Snake([(5,5), (5,6), (5,7)], (-1, 0))
+    print(snake2.body)
+    print(snake2.direction)
+    # lấy đầu con rắn
+    print(snake2.body[0])
+
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\nTrò chơi bị dừng.")
