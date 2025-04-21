62b667e596befd5522d851ddc57806061c43ca41
Quang Nguyễn <207350182+nngocquangUIT24730135@users.noreply.github.com>
2025-04-21 20:55:12 +0700

    Them print message

--- game.py
+++ game.py
@@ -1,6 +1,5 @@
 import os
 import time
-import keyboard
 import random
 
 # Cài đặt trò chơi
@@ -28,21 +27,22 @@ class Snake:
 def main():
     # Tạo một con rắn với đối số mặc định
     snake = Snake()
-    print(snake.body)
-    print(snake.direction)
+    print("Rắn 1 - Vị trí khởi tạo:", snake.body)
+    print("Rắn 1 - Hướng di chuyển:", snake.direction)
 
     # Kiểm tra thông tin về kiểu dữ liệu
-    print(type(snake.body))  # <class 'list'
-    print(type(snake.body[0])) # <class 'tuple'>
-    print(type(snake.direction)) # <class 'tuple'>
+    print("\n[Kiểu dữ liệu]")
+    print("Kiểu snake.body:", type(snake.body))           # <class 'list'>
+    print("Kiểu phần tử đầu trong body:", type(snake.body[0]))  # <class 'tuple'>
+    print("Kiểu direction:", type(snake.direction))       # <class 'tuple'>
 
     # Tạo con rắn thứ 2 có độ dài là 3 và hướng di chuyển sang trái
     snake2 = Snake([(5,5), (5,6), (5,7)], (-1, 0))
-    print(snake2.body)
-    print(snake2.direction)
-    # lấy đầu con rắn
-    print(snake2.body[0])
+    print("\nRắn 2 - Vị trí khởi tạo:", snake2.body)
+    print("Rắn 2 - Hướng di chuyển:", snake2.direction)
 
+    # Lấy đầu con rắn
+    print("Rắn 2 - Vị trí đầu rắn:", snake2.body[0])
 
 if __name__ == "__main__":
     try:
